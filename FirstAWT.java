7acebf2e757594039cb455dcd6d4dd516a1ea596
geekyneha <nehatualea@gmail.com>
2025-12-30 21:12:06 +0530

    Java AWT

---
+++ FirstAWT.java
@@ -0,0 +1,15 @@
+
+import java.awt.*;
+
+public class FirstAWT{
+    public static void main(String args[]){
+        Frame f = new Frame("My First AWT Frame");
+        Button b = new Button("Click Me");
+        b.setBounds(50,100,80,30);
+        f.add(b);
+       
+        f.setSize(300, 200);
+        f.setVisible(true);
+
+    }
+}
